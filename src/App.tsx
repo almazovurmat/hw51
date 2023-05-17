9f2e16d916ebceb9e8bd7a8719b649f5b61e44bc
Almazov Urmat <almazov.urmat@gmail.com>
2023-05-17 13:40:44 +0600

    import Number in App, created function for generate 5 numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Number from "./Number/Number";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [arrayNumbers, setNumbers] = useState<number[]>([]);
 
+    const getArrayNumbers = () => {
+        const generateNumber = () => {
+            return Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+        };
+
+        let generatedNumbers: number[] = [];
+
+        while (generatedNumbers.length < 5) {
+            const number: number = generateNumber();
+            if (!generatedNumbers.includes(number)) {
+                generatedNumbers.push(number);
+            }
+        }
+
+        setNumbers(generatedNumbers);
+    };
+
+    return (
+        <div className="App">
+            <button onClick={getArrayNumbers}>New numbers</button>
+            <div className="numbersBlock">
+                {
+                    arrayNumbers.map(number => {
+                        return <Number key={number} number={number}/>;
+                    })
+                }
+            </div>
+        </div>
+    );
+};
 export default App;
